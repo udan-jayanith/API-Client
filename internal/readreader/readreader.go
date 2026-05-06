36b1f701f9a3a48bbe5477e430d31cab8413036b
Udan Jayanith <j.udanjayanith@gmail.com>
2026-05-06 10:39:18 +0530

    Add comment

---
+++ internal/readreader/readreader.go
@@ -0,0 +1,29 @@
+package decompressor
+
+// See: https://pkg.go.dev/encoding/csv
+
+import (
+	"io"
+	"os"
+	// "compress/gzip" // Has a read closer
+	// "compress/flate" // Also has a read closer
+	// "github.com/andybalholm/brotli" // br // has a reader, i think this may be closed but dosen't implement io.Closer
+	// "github.com/klauspost/compress/zstd" // has a reader, i think this may be closed but dosen't implement io.Closer
+)
+
+// This implements Io.ReadCloser
+type ReadReader struct {
+	buf  []byte
+	file os.File
+}
+
+// NewReader returns a new io.Reader that reads from r's internal buffer. This does not clear the internal buffer from r
+func (r *ReadReader) NewReader() io.ReadCloser {
+	return nil
+}
+
+// NewReadReader returns a new *ReadReader, that reads form r, if there are any compressions r get decompressed in order.
+// content reads form r gets stored in a internal buffer if internal buffer size exceeds 20mb buffer will be a file in OS temporory directory.
+func NewReadReader(r io.Reader, compressions []string) *ReadReader {
+	return nil
+}
