9fcee24550ed118cfb6e2d93da98c2d0b44dc39c
Udan Jayanith <j.udanjayanith@gmail.com>
2026-05-06 21:52:05 +0530

    Add comments

--- internal/readreader/readreader.go
+++ internal/readreader/readreader.go
@@ -3,18 +3,29 @@ package decompressor
 // See: https://pkg.go.dev/encoding/csv
 
 import (
+	"bytes"
 	"io"
 	"os"
 	// "compress/gzip" // Has a read closer
 	// "compress/flate" // Also has a read closer
-	// "github.com/andybalholm/brotli" // br // has a reader, i think this may be closed but dosen't implement io.Closer
-	// "github.com/klauspost/compress/zstd" // has a reader, i think this may be closed but dosen't implement io.Closer
+	// "github.com/andybalholm/brotli" // br // has a reader
+	// "github.com/klauspost/compress/zstd" // has a reader, reader should closed but dosen't implement io.Closer
 )
+
+type closer interface {
+	Close()
+}
+
+// r_heandler is returned by ReadReader's NewReader. If r hasn't read fully
+// this stores content from r into appropriate buffers. If buf of ReadReader is not empty NewReader must store a copy of bytes.Buffer.
+// If the buufer buffer is a file pointer to the file must store as well.
+type read_handler struct {
+}
 
 // This implements Io.ReadCloser
 type ReadReader struct {
-	r io.ReadCloser
-	buf  []byte
+	r    io.Reader
+	buf  *bytes.Buffer
 	file os.File
 }
 
@@ -24,7 +35,7 @@ func (r *ReadReader) NewReader() io.ReadCloser {
 }
 
 // NewReadReader returns a new *ReadReader, that reads form r, if there are any compressions r get decompressed in order.
-// content reads form r gets stored in a internal buffer if internal buffer size exceeds 2mb buffer will be a file in OS temporory directory.
+// content reads form r gets stored in an internal buffer. If internal buffer size exceeds 2mb buffer will be a file in OS temporory directory. If r is a io.ReadCloser ReadReader handle the closing.
 func NewReadReader(r io.Reader, compressions []string) *ReadReader {
 	return nil
 }
