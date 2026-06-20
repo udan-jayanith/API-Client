229d4de8518f4d0b3757b792f9ea9c0c80003431
Udan Jayanith <j.udanjayanith@gmail.com>
2026-06-20 14:29:36 +0530

    changes

--- pages/request/internal/content-encoding/content-encoding.go
+++ pages/request/internal/content-encoding/content-encoding.go
@@ -12,35 +12,49 @@ import (
 	"Zbolt/pages/request/internal/content-encoding/compress/deflate"
 	"Zbolt/pages/request/internal/content-encoding/compress/gzip"
 	"Zbolt/pages/request/internal/content-encoding/compress/zstd"
+	"errors"
 
 	"io"
 )
 
+// Decode may close r
 func Decode(r io.Reader, encodings []string) (readreader.ReadReader, error) {
-	//rr := readreader.NewReadReader(readreader.DefualtSize, make([]byte, 0, 2024))
+	closers := make([]io.ReadCloser, 0, len(encodings))
+	closer, ok := r.(io.ReadCloser)
+	if ok {
+		closers = append(closers, closer)
+	}
+
+	var err error
+loop:
 	for _, encoding := range encodings {
 		switch encoding {
 		case "gzip":
-			_, err := gzip.Decompress(r)
+			_, err = gzip.Decompress(r)
 			if err != nil {
-				return readreader.ReadReader{}, err
+				break loop
 			}
 		case "deflate":
-			_, err := deflate.Decompress(r)
+			_, err = deflate.Decompress(r)
 			if err != nil {
-				return readreader.ReadReader{}, err
+				break loop
 			}
 		case "br":
-			_, err := br.Decompress(r)
+			_, err = br.Decompress(r)
 			if err != nil {
-				return readreader.ReadReader{}, err
+				break loop
 			}
 		case "zstd":
-			_, err := zstd.Decompress(r)
+			_, err = zstd.Decompress(r)
 			if err != nil {
-				return readreader.ReadReader{}, err
+				break loop
 			}
+		default:
+			err = errors.New("Unknown encoding")
+			break loop
 		}
 	}
-	return readreader.ReadReader{}, nil
+
+	// read from r and write it to rr.
+	return readreader.ReadReader{}, err
 }
